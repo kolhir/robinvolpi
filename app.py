ee3cb99d43b6b5db558ea9e7c9d09147afdb02c7
kolhir <kolhir16@gmail.com>
2019-05-20 16:57:37 +0400

    first

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from config import Confiruration
+import json
+app = Flask(__name__)
+app.config.from_object(Confiruration)
+
+
+@app.route("/test")
+def test():
+    return render_template("index.html")
+
+@app.route("/bronirovanie", methods = ['POST'])
+def ajax_bron():
+	return json.dumps(200)
+# from users import models, views
